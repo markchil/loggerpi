6df2958eebad579904b09e5800e1bdbc72234bba
markchil <mark.chilenski@gmail.com>
2019-09-18 19:59:02 -0400

    Reverse buffer, update plot.

--- loggerpi.py
+++ loggerpi.py
@@ -10,7 +10,8 @@ UNITS = W1ThermSensor.DEGREES_F
 
 sensor = W1ThermSensor()
 temperature_buffer = np.nan * np.ones(BUFFER_LENGTH)
-time_grid = np.arange(BUFFER_LENGTH) * -1 * UPDATE_INTERVAL / 60.0
+time_grid = np.arange(BUFFER_LENGTH) * UPDATE_INTERVAL / 60.0
+time_grid = -1 * time_grid[::-1]
 figure, axis = plt.subplots()
 axis.set_xlabel('Time Relative to Now [min]')
 axis.set_ylabel('Temperature [°F]')
@@ -19,8 +20,9 @@ line, = axis.plot(time_grid, temperature_buffer, '.--')
 while True:
     temperature = sensor.get_temperature(UNITS)
     print("The current temperature is {:.1f}°F".format(temperature))
-    temperature_buffer[1:] = temperature_buffer[0:-1]
-    temperature_buffer[0] = temperature
+    temperature_buffer[0:-1] = temperature_buffer[1:]
+    temperature_buffer[-1] = temperature
     line.set_ydata(temperature_buffer)
+    figure.canvas.draw_idle()
     figure.savefig('temperature.png')
     sleep(UPDATE_INTERVAL)
