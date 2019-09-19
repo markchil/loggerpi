79d7c88a7ea5f8f9129b9995c801f9666fd55eec
markchil <mark.chilenski@gmail.com>
2019-09-18 21:17:53 -0400

    Only update plot now and then.

--- loggerpi.py
+++ loggerpi.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 from matplotlib.dates import date2num
 from w1thermsensor import W1ThermSensor
 
-UPDATE_INTERVAL = 1.0
+UPDATE_INTERVAL = 2.0
+PLOT_UPDATE_STEP_INTERVAL = 10
 BUFFER_DURATION = 60.0 * 60.0 * 1.0
 BUFFER_LENGTH = int(np.ceil(BUFFER_DURATION / UPDATE_INTERVAL))
 UNITS = W1ThermSensor.DEGREES_F
@@ -29,17 +30,21 @@ axes.set_ylabel('Temperature [°F]')
 axes.set_title(HOSTNAME)
 line, = axes.plot_date(time_grid, temperature_buffer, '.--')
 
+steps = 0
 while True:
     temperature = sensor.get_temperature(UNITS)
     timestamp = date2num(datetime.now())
     print("The current temperature is {:.1f}°F".format(temperature))
     update_buffer(temperature_buffer, temperature)
     update_buffer(time_grid, timestamp)
-    line.set_xdata(time_grid)
-    line.set_ydata(temperature_buffer)
-    axes.relim()
-    axes.autoscale_view()
-    figure.autofmt_xdate()
-    figure.canvas.draw_idle()
-    figure.savefig('temperature.png', bbox_inches='tight')
+    if steps % PLOT_UPDATE_STEP_INTERVAL == 0:
+        print("Updating plot...")
+        line.set_xdata(time_grid)
+        line.set_ydata(temperature_buffer)
+        axes.relim()
+        axes.autoscale_view()
+        figure.autofmt_xdate()
+        figure.canvas.draw_idle()
+        figure.savefig('temperature.png', bbox_inches='tight')
+    steps += 1
     sleep(UPDATE_INTERVAL)
